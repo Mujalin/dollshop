6adfe421aed22655252bc49ebd3fdbe00ea54a29
Mujalin <62310021@my.buu.ac.th>
2021-10-20 23:04:24 +0700

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,29 @@
+import { HomeIcon } from "@heroicons/react/solid";
+import React from "react";
+import { projects } from "../data";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+      <HomeIcon className="w-10 inline-block mb-4" />
+        <div className="flex flex-col w-full mb-20">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+            welcome to my Shop
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            นี่เป็นตัวอย่างร้านค้าของเรา
+            หากสนใจสั่งซื้อกดปุ่ม สั่งซื้อ ได้เลย
+            <br></br>
+            <br></br>
+            <a
+              href="index2.js"
+              className="inline-flex  text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded ">
+              สั่งซื้อ
+            </a>
+          </p>
+        </div> 
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,81 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "ผมมีความยินดีเป็นอย่างยิ่งที่คุณใช้บริการร้านของเรา หวังว่าคุณจะชอบสินค้าของเรา",
+    image: "https://scontent.fbkk6-2.fna.fbcdn.net/v/t1.6435-9/66466476_109309573707109_8061099245983236096_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGd1isv4MLannv3CcfOGRUVwFegJ1ZUwsnAV6AnVlTCyfsF4tz5vWbpnP57Z-5lSenbeXv0YPUNQR3CMMdYtUj_&_nc_ohc=6Ge6qNMfbPQAX-A88SP&_nc_ht=scontent.fbkk6-2.fna&oh=210cbb3393c5371b139f5538854d7f22&oe=619385C7",
+    name: "นายกิตติศักดิ์ แก้วทอง",
+    company: "ผู้จัดการร้านบูรพากิฟท์ช็อป",
+  },
+  {
+    quote:
+      "ลูกค้าคือพระเจ้า ร้านของเรายินดีต้อนรับ",
+    image: "https://scontent.fbkk6-1.fna.fbcdn.net/v/t1.6435-9/83904898_2819587351601426_7127529499368882176_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGmV8jax2rXXV56veB5if6k2w13nhC-H8LbDXeeEL4fwjZNOAzGB4AmgrC_7jY2pHsRD3UuRAqnAWIzBzKTfTB0&_nc_ohc=DtJKTv99ADMAX-RFbrE&_nc_ht=scontent.fbkk6-1.fna&oh=6cef9a9a96a902d77314ef396b263f1d&oe=619556BD",
+    name: "นายพงศ์ภรณ์ จันทาพูน",
+    company: "ผู้ช่วยผู้จัดการร้านบูรพากิฟท์ช็อป",
+  },
+  {
+    quote:
+      "ฉันขอบคุณทุกคนที่ชอบ งานฝีมือของฉันขอให้ทุกคนมีความสุขกับงานของฉัน",
+    image: "https://scontent.fbkk6-1.fna.fbcdn.net/v/t1.6435-9/186893101_101954598756783_5373669185090059318_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGJfJ8EdI_CJye6V626DvqTXIvEBe9VTd1ci8QF71VN3dIQerfffAoqFeXn_glsQFM3FmNN2WsIZHG7YfN1Bxl_&_nc_ohc=J27Z7mTJGk8AX-eagFB&_nc_ht=scontent.fbkk6-1.fna&oh=3bfdb4c438ab7edfdc38ac4c8064463e&oe=6196A7BC",
+    name: "นางสาวมุจลินท์ ขัติยนนท์",
+    company: "พนักงานร้านบูรพากิฟท์ช็อป",
+  },
+  {
+    quote:
+      "ให้ตุ้กตาแทนการบอกว่าคุณรักเขาแค่ไหน",
+    image: "https://scontent.fbkk6-2.fna.fbcdn.net/v/t1.6435-9/158314317_1903862213094348_1484783733662586558_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeH-SndJhOM072Snjl7fRip-yB-fPb56SivIH589vnpKK-W2Fb4zLduzLIo6Uvshj_ZQiFUrhevUHuRLd0rX-C0s&_nc_ohc=fs46WyY0OCAAX_YR1xG&_nc_ht=scontent.fbkk6-2.fna&oh=68f875608021fc0d78cff70143a59e83&oe=6193DF74",
+    name: "นายสุชาพงศ์ ทรงประโคน",
+    company: "พนักงานขายร้านบูรพากิฟท์ช็อป",
+  },
+  {
+    quote:
+      "คุ้กตาที่คุณให้เขา เขายังเก็บมันอยู่ไหม",
+    image: "https://scontent.fbkk6-1.fna.fbcdn.net/v/t1.6435-9/159320041_2947784612109471_6357569492721761327_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFUmKVIPA9jgQKZIIYAkP2Z-F0v428An_f4XS_jbwCf97tsmotw5RB1uVoDzHgry6lR72WXKvzFMVc0CDRATI4Y&_nc_ohc=ch0_OmvznR8AX8znrx-&_nc_ht=scontent.fbkk6-1.fna&oh=ab96586e2719e88d4f4004351688aff5&oe=61970184",
+    name: "นายอภิธาร บู่สามสาย",
+    company: "หุ้นส่วนร้านบูรพากิฟท์ช็อป",
+  },
+];
+
+export const skills = [
+  "ร้านค้ารับประกันสินค้าเป็นแบบทำมือ",
+  "หากมีการชำรุดร้านค้าจะเปลี่ยนให้ฟรี",
+  "ร้านค้าตั้งอยู่ที่ เลขที่ 57 หมู่ 1 โขมง อำเภอท่าใหม่ จันทบุรี 22170",
+  "เบอร์ติดต่อ +6639310000",
+  "เวลาเปิด/ปิด 08.00-04.30 น.",
+  "รับประกันรคุณภาพ",
+];
