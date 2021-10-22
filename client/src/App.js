75a4de75c17acf9ea69a3094e52701ac37aa243e
AtoMiC150 <62310071@go.buu.ac.th>
2021-10-22 21:58:51 +0700

    product

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import ListCom from "./components/ListCom";
 import Navbar from "./components/Navbar";
 import Product from "./components/Product";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
+import './qq.css';
 
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
+      {/* <Navbar />
       <Product/>
       <About />
       <Projects />
       <Skills />
       <Testimonials />
-      <Contact />
+      <Contact /> */}
+      <ListCom/>
     </main>
   );
 }
--- client/src/components/ListCom.js
+++ client/src/components/ListCom.js
@@ -1,61 +1,62 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 export default function ListCom() {
-    const [pro_name,setpro_name]=useState(["ตุ๊กตา"])
-    const [productList,setproductList]=useState([])
-    const loadList=async()=>{
+    const [pro_name, setpro_name] = useState(["ตุ๊กตา"])
+    const [productList, setproductList] = useState([])
+    const loadList = async () => {
         try {
 
-            const resp=await fetch(`http://localhost:4000/getProduct/${pro_name}`)
-            const jsondata=await resp.json()
+            const resp = await fetch(`http://localhost:4000/getProduct/${pro_name}`)
+            const jsondata = await resp.json()
             setproductList(jsondata)
             console.log(productList)
-            
+
 
         } catch (error) {
             console.error(error.message)
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         loadList();
 
-    },[pro_name])
+    }, [pro_name])
 
     // const pro=productList.map(x=>{
     //     return [x.pro_id,x.pro_name]
-        
+
     // })
     // console.log(pro)
 
     return (
         <div>
-                        <input type="name" class="form-control" placeholder="Enter name" id="name" 
-                    onChange={e=>{
-                        setpro_name(e.target.value)
-                    }}
-                    />
-            {productList.map((products)=>{
-                return(
+            <input type="name" class="form-control" placeholder="Enter name" id="name"
+                onChange={e => {
+                    setpro_name(e.target.value)
+                }}
+            />
+            {productList.map((products) => {
+                const pro_img="./product/"+products.pro_img
+                return (
 
-                <>
-    <div class="at">
-<div className="card"> 
-  <a href="produce.js">
-    <img className="card-img-top" src={products.pro_img} />
-  </a>
-  <div className="card-body">
-    <h5 className="card-title">{products.pro_name}</h5>
-    <p className="card-text"> ราคา {products.pro_price} ขนาด {products.pro_size}</p>
-  </div>
-</div>
+                    
+                        <div class="at">
+                            <div className="card">
+                                <a href="produce.js">
+                                    <img className="card-img-top" src={pro_img} />
+                                </a>
+                                <div className="card-body">
+                                    <h5 className="card-title">{products.pro_name}</h5>
+                                    <p className="card-text"> ราคา {products.pro_price} ขนาด {products.pro_size}</p>
+                                </div>
+                            </div>
 
-    </div>
-                </>
+                        </div>
+                    
                 )
             })}
         </div>
-        
+
     )
-    
+
 }
